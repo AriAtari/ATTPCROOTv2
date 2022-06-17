c749464cc09aa2814ee7fff2120f7ab1ae6bbe0f
Arian Andalib <Andalib@nscl.msu.edu>
2022-06-17 19:00:04 -0400

    initial commit

--- macro/Simulation/SeGA/sim_analysis.C
+++ macro/Simulation/SeGA/sim_analysis.C
@@ -4,7 +4,7 @@
 void sim_analysis(Int_t num_ev=10000)
 {
 
-    TString mcFileNameHead = "SeGA";
+    TString mcFileNameHead = "./data/SeGA";
     TString mcFileNameTail = ".root";
     TString mcFileName     = mcFileNameHead + mcFileNameTail;
     TString outFileNameHead = "SeGAana";
@@ -31,7 +31,7 @@ void sim_analysis(Int_t num_ev=10000)
     TH2D *ThetaLabSim_vs_Z = new TH2D("ThetaLabSim_vs_Z","ThetaLabSim_vs_Z",1000,0,-1000,1000,-180,180);
 
       TCanvas *c1 = new TCanvas();
-    c1->Divide(2,2);
+    c1->Divide(1,1);
     c1->Draw();
 
     for(Int_t iEvent=0; iEvent<nEvents; iEvent++)
@@ -54,22 +54,22 @@ void sim_analysis(Int_t num_ev=10000)
            for(Int_t i=0; i<n; i++) {
 
             point = (AtSeGAPoint*) pointArray -> At(i);
-            auto VolName=point->GetVolName();
+            auto VolName="germaniumTarget";
             auto trackID = point -> GetTrackID();
             //std::cout<<" Volume Name : "<<VolName<<std::endl;
             //std::cout<<" Track ID : "<<trackID<<std::endl;
 
              if(trackID==1) //Scattered proton
 	     {
-              if(VolName!="germanium"){
+              if(VolName!="germaniumTarget"){
 	       std::cout<<" Volume Name : "<<VolName<<std::endl;
                std::cout<<" Track ID : "<<trackID<<std::endl;
                std::cout<<" Point number : "<<i<<std::endl;
 	       energyLoss_sca+=( point -> GetEnergyLoss() )*1000;//MeV
 	       zpos_sca=point->GetZ()*10;
-               angle_sca=point->GetAIni();
+               //angle_sca=point->GetAIni();
 	       std::cout<<" Energy Loss "<<energyLoss_sca<<"  Z pos "<<zpos_sca<<" Point ID : "<<i<<"\n";
-               }else if(VolName=="siliconTarget"){
+               }else if(VolName=="germaniumTarget"){
                  
                  energyLossTarget_sca+=( point -> GetEnergyLoss() )*1000;//MeV;
                }
@@ -78,14 +78,14 @@ void sim_analysis(Int_t num_ev=10000)
 
              if(trackID==0) //Heavy recoil
 	     {
-	      if(VolName!="germainium"){
+	      if(VolName!="germaniumTarget"){
 	       std::cout<<" Volume Name : "<<VolName<<std::endl;
                std::cout<<" Track ID : "<<trackID<<std::endl;
                std::cout<<" Point number : "<<i<<std::endl;
 	       energyLoss_rec+=( point -> GetEnergyLoss() )*1000;//MeV
 	       zpos_rec=point->GetZ()*10;
-               xpos_rec=point->GetX(zpos_rec)*10;
-               ypos_rec=point->GetY(zpos_rec)*10;
+               xpos_rec=point->GetX()*10;
+               ypos_rec=point->GetY()*10;
 	       //std::cout<<" Energy Loss "<<energyLoss_rec<<"  Z pos "<<zpos_rec<<" Point ID : "<<i<<"\n";
               }
 	     }
@@ -93,19 +93,19 @@ void sim_analysis(Int_t num_ev=10000)
 
            }
 
-	Eloss_vs_Z->Fill(zpos_sca,energyLoss_sca);
-        Recoil_XY->Fill(xpos_rec,ypos_rec);
-        ThetaLabSim_vs_Z->Fill(zpos_sca,angle_sca);
+	//Eloss_vs_Z->Fill(zpos_sca,energyLoss_sca);
+        //Recoil_XY->Fill(xpos_rec,ypos_rec);
+       // ThetaLabSim_vs_Z->Fill(zpos_sca,angle_sca);
 
         ElossTarget_vs_Z->Fill(zpos_sca,energyLossTarget_sca);
 
     }
     
+   // c1->cd(1);
+  //  Eloss_vs_Z->Draw("zcol");
+   // c1->cd(2);
+  //  Recoil_XY->Draw("zcol");
     c1->cd(1);
-    Eloss_vs_Z->Draw("zcol");
-    c1->cd(2);
-    Recoil_XY->Draw("zcol");
-    c1->cd(3);
     ElossTarget_vs_Z->Draw("zcol");
 
 }
