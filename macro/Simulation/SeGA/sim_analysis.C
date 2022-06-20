d8aea31b8af074a24a411e0ab0372a134533f1a6
Arian Andalib <Andalib@nscl.msu.edu>
2022-06-20 06:25:42 -0400

    energy loss hist added

--- macro/Simulation/SeGA/sim_analysis.C
+++ macro/Simulation/SeGA/sim_analysis.C
@@ -7,7 +7,7 @@ void sim_analysis(Int_t num_ev=10000)
     TString mcFileNameHead = "./data/SeGA";
     TString mcFileNameTail = ".root";
     TString mcFileName     = mcFileNameHead + mcFileNameTail;
-    TString outFileNameHead = "SeGAana";
+    TString outFileNameHead = "./data/SeGAana";
     TString outFileNameTail = ".root";
     TString outFileName     = outFileNameHead + outFileNameTail;
 
@@ -25,13 +25,11 @@ void sim_analysis(Int_t num_ev=10000)
     if(nEvents>num_ev) nEvents=num_ev;
 
     //Histograms
-    TH2D *Eloss_vs_Z = new TH2D("Eloss_vs_Z","ELoss_vs_Z",1000,0,-1000,1000,0,10);
-    TH2D *ElossTarget_vs_Z = new TH2D("ElossTarget_vs_Z","ELossTarget_vs_Z",1000,0,-1000,1000,0,10);
-    TH2D *Recoil_XY = new TH2D("Recoil_XY","Recoil_XY",1000,-50.0,50.0,1000,-50.0,50.0);
-    TH2D *ThetaLabSim_vs_Z = new TH2D("ThetaLabSim_vs_Z","ThetaLabSim_vs_Z",1000,0,-1000,1000,-180,180);
+    TH1D *Eloss_vs_events= new TH1D("Eloss_vs_events","ELoss_vs_events",20,0,20);
+    
 
       TCanvas *c1 = new TCanvas();
-    c1->Divide(1,1);
+    //c1->Divide(2,2);
     c1->Draw();
 
     for(Int_t iEvent=0; iEvent<nEvents; iEvent++)
@@ -54,14 +52,15 @@ void sim_analysis(Int_t num_ev=10000)
            for(Int_t i=0; i<n; i++) {
 
             point = (AtSeGAPoint*) pointArray -> At(i);
-            auto VolName="germaniumTarget";
+            auto VolName="germanium";
+
             auto trackID = point -> GetTrackID();
-            //std::cout<<" Volume Name : "<<VolName<<std::endl;
+           // std::cout<<" Volume Name : "<<VolName<<std::endl;
             //std::cout<<" Track ID : "<<trackID<<std::endl;
 
              if(trackID==1) //Scattered proton
 	     {
-              if(VolName!="germaniumTarget"){
+              if(VolName!="germanium"){
 	       std::cout<<" Volume Name : "<<VolName<<std::endl;
                std::cout<<" Track ID : "<<trackID<<std::endl;
                std::cout<<" Point number : "<<i<<std::endl;
@@ -69,7 +68,7 @@ void sim_analysis(Int_t num_ev=10000)
 	       zpos_sca=point->GetZ()*10;
                //angle_sca=point->GetAIni();
 	       std::cout<<" Energy Loss "<<energyLoss_sca<<"  Z pos "<<zpos_sca<<" Point ID : "<<i<<"\n";
-               }else if(VolName=="germaniumTarget"){
+               }else if(VolName=="germanium"){
                  
                  energyLossTarget_sca+=( point -> GetEnergyLoss() )*1000;//MeV;
                }
@@ -78,7 +77,7 @@ void sim_analysis(Int_t num_ev=10000)
 
              if(trackID==0) //Heavy recoil
 	     {
-	      if(VolName!="germaniumTarget"){
+	      if(VolName!="germanium"){
 	       std::cout<<" Volume Name : "<<VolName<<std::endl;
                std::cout<<" Track ID : "<<trackID<<std::endl;
                std::cout<<" Point number : "<<i<<std::endl;
@@ -86,26 +85,28 @@ void sim_analysis(Int_t num_ev=10000)
 	       zpos_rec=point->GetZ()*10;
                xpos_rec=point->GetX()*10;
                ypos_rec=point->GetY()*10;
-	       //std::cout<<" Energy Loss "<<energyLoss_rec<<"  Z pos "<<zpos_rec<<" Point ID : "<<i<<"\n";
+	       std::cout<<" Energy Loss "<<energyLoss_rec<<"  Z pos "<<zpos_rec<<" Point ID : "<<i<<"\n";
               }
 	     }
 				
 
            }
 
-	//Eloss_vs_Z->Fill(zpos_sca,energyLoss_sca);
+	Eloss_vs_events->Fill(iEvent,energyLossTarget_sca);
         //Recoil_XY->Fill(xpos_rec,ypos_rec);
-       // ThetaLabSim_vs_Z->Fill(zpos_sca,angle_sca);
+        //ThetaLabSim_vs_Z->Fill(zpos_sca,angle_sca);
 
-        ElossTarget_vs_Z->Fill(zpos_sca,energyLossTarget_sca);
+        //ElossTarget_vs_Z->Fill(zpos_sca,energyLossTarget_sca);
+std::cout<<energyLossTarget_sca<<","<<iEvent<<endl;
 
     }
-    
-   // c1->cd(1);
-  //  Eloss_vs_Z->Draw("zcol");
-   // c1->cd(2);
-  //  Recoil_XY->Draw("zcol");
+   // std::sort(x,x+20);
     c1->cd(1);
-    ElossTarget_vs_Z->Draw("zcol");
+    Eloss_vs_events->Draw("BAR");
+   // c1->cd(2);
+   // Recoil_XY->Draw("zcol");
+   // c1->cd(1);
+  //  auto g = new TGraph(no,x,y);
+   //g->Draw("A*");
 
 }
